395f1844454ecda0b6fff1af47077bd5f1a82161
iamkashifkhn <usmankashif544@gmail.com>
2022-02-18 11:35:00 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ApiFetch from "./components/ApiFetch";
+// import AscendingOrder from "../src/components/AscendingOrder/AscendingOrder";
+// import ReverseOrder from "./components/ReverseOrder/ReverseOrder";
+
+function App() {
+  return (
+    <div className="container gifs">
+      <ApiFetch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ApiFetch.jsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from "react";
+import "./gifs.css";
+import Button from "../../node_modules/@material-ui/core/Button/Button";
+
+function ApiFetch() {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [reverse, setReverse] = useState(false);
+
+  const onClickHandle = () => {
+    setReverse(!reverse);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setLoading(true);
+
+      try {
+        const result = await fetch(
+          `https://api.giphy.com/v1/gifs/trending?api_key=xQ1dCumvjQcPPojDWO2OLO99a3HoG2bw`
+        );
+        const actualData = await result.json();
+        setData(actualData.data);
+        console.log(actualData.data[0].username);
+      } catch (error) {
+        console.log("error", error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+  useEffect(() => {
+    setData(data.reverse());
+  }, [reverse]);
+
+  return (
+    <>
+      <div className="searchbar">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "left",
+            alignItems: "center",
+            width: "30%",
+            borderBottom: "1px solid rgb(134, 31, 31)",
+          }}
+        >
+          &#128270;
+          <input
+            type="search"
+            placeholder="Search by Name or Title"
+            className="search"
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+        </div>
+        <div className="myButton">
+          <div className="favorite">
+            <Button variant="contained">&#9733; Favorites </Button>
+          </div>
+          <div className="myOrder">
+            {reverse ? (
+              <Button variant="contained" onClick={onClickHandle}>
+                &uarr; Ascending
+              </Button>
+            ) : (
+              <Button variant="contained" onClick={onClickHandle}>
+                &darr; Descending
+              </Button>
+            )}
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <div className="gifs">
+        {data
+          .filter((current) => {
+            if (
+              current.title.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return current;
+            } else {
+              return console.log(current);
+            }
+          })
+          .map((val, idx) => {
+            return (
+              <div key={idx} className="gif">
+                <img src={val.images.fixed_height.url} />
+                <div className="showDetails">
+                  <p style={{ color: "white" }}>
+                    <span
+                      style={{
+                        color: "red",
+                        fontWeight: "bold",
+                        marginRight: "3px",
+                      }}
+                    >
+                      Name:
+                    </span>
+                    {val?.username}
+                  </p>
+                  <p style={{ color: "white" }}>
+                    <span
+                      style={{
+                        color: "red",
+                        fontWeight: "bold",
+                        marginRight: "3px",
+                      }}
+                    >
+                      Rating:
+                    </span>
+                    {val?.rating}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+}
+
+export default ApiFetch;

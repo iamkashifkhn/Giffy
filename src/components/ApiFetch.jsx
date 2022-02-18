6a61b0a67de557253a2aa6ae96f219b10ec5f712
iamkashifkhn <usmankashif544@gmail.com>
2022-02-18 23:20:49 +0500

    first commit

--- src/components/ApiFetch.jsx
+++ src/components/ApiFetch.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./gifs.css";
 import Button from "../../node_modules/@material-ui/core/Button/Button";
-// import Loader from "../components/Loader/loader";
 
 function ApiFetch() {
   const [data, setData] = useState([]);
@@ -9,7 +8,14 @@ function ApiFetch() {
   const [loading, setLoading] = useState(true);
   const [reverse, setReverse] = useState(false);
   const [expand, setExpend] = useState(false);
-  var favArr = [];
+  const [show, setShow] = useState(false);
+  const [fData, setFData] = useState(null);
+  useEffect(() => {
+    setFData(JSON.parse(localStorage.getItem("data")));
+    // const a = JSON.parse(localStorage.getItem("data"));
+    // console.log(JSON.parse(localStorage.getItem("data")));
+  }, [show, favArr]);
+  var favArr = JSON.parse(localStorage.getItem("data"));
 
   const onClickHandle = () => {
     setReverse(!reverse);
@@ -21,6 +27,7 @@ function ApiFetch() {
 
   const addFavGif = (val) => {
     favArr.unshift(val);
+    localStorage.setItem("data", JSON.stringify(favArr));
     console.log(favArr);
   };
 
@@ -60,8 +67,17 @@ function ApiFetch() {
         </div>
         <div className="myButton">
           <div className="favorite">
-            <Button variant="contained">&#9733; Favorites </Button>
+            <Button variant="contained" onClick={() => setShow(false)}>
+              &#8701; Home
+            </Button>
           </div>
+
+          <div className="favorite">
+            <Button variant="contained" onClick={() => setShow(true)}>
+              &#9733; Favorites
+            </Button>
+          </div>
+
           <div className="myOrder">
             {reverse ? (
               <Button variant="contained" onClick={onClickHandle}>
@@ -77,50 +93,84 @@ function ApiFetch() {
       </div>
       <br />
       <br />
-      <div className="gifs">
-        {data
-          .filter((current) => {
-            if (
-              current.title.toLowerCase().includes(searchTerm.toLowerCase())
-            ) {
-              return current;
-            } else {
-              return console.log(current);
-            }
-          })
-          .map((val, idx) => {
-            return (
-              <div key={idx} className="gif">
-                {loading ? (
-                  <h1> Loading .... </h1>
-                ) : (
-                  <img src={val.images.fixed_height.url} onClick={expendIt} />
-                )}
-                {expand ? (
-                  <div className="showDetails">
-                    <p style={{ color: "white" }}>
-                      <span className="names">Name:</span>
-                      {val?.username}
-                    </p>
-                    <p style={{ color: "white" }}>
-                      <span className="names">Rating:</span>
-                      {val?.rating}
-                    </p>
-                    <Button
-                      variant="contained"
-                      onClick={() => {
-                        addFavGif(val);
-                      }}
-                      className="favButton"
-                    >
-                      Fav
-                    </Button>
-                  </div>
-                ) : null}
-              </div>
-            );
-          })}
-      </div>
+      {show ? (
+        <div className="gifs">
+          {fData &&
+            fData.map((data, index) => {
+              return (
+                <div key={index} className="gif">
+                  {loading ? (
+                    <h1> Loading .... </h1>
+                  ) : (
+                    <img
+                      src={data?.images?.fixed_height?.url}
+                      onClick={expendIt}
+                    />
+                  )}
+                  {expand ? (
+                    <div className="showDetails">
+                      <p style={{ color: "white" }}>
+                        <span className="names">Name:</span>
+                        {data?.username}
+                      </p>
+                      <p style={{ color: "white" }}>
+                        <span className="names">Rating:</span>
+                        {data?.rating}
+                      </p>
+                    </div>
+                  ) : null}
+                </div>
+              );
+            })}
+        </div>
+      ) : (
+        <div className="gifs">
+          {data
+            .filter((current) => {
+              if (
+                current.title.toLowerCase().includes(searchTerm.toLowerCase())
+              ) {
+                return current;
+              } else {
+                return console.log(current);
+              }
+            })
+            .map((val, idx) => {
+              return (
+                <div key={idx} className="gif">
+                  {loading ? (
+                    <h1> Loading .... </h1>
+                  ) : (
+                    <img src={val.images.fixed_height.url} onClick={expendIt} />
+                  )}
+                  {expand ? (
+                    <div className="showDetails">
+                      <div className="details">
+                        <p>
+                          <span className="names">Name:</span>
+                          {val?.username}
+                        </p>
+                        <p>
+                          <span className="names">Rating:</span>
+                          {val?.rating}
+                        </p>
+                        <Button
+                          variant="contained"
+                          onClick={() => {
+                            addFavGif(val);
+                          }}
+                          className="favButton"
+                        >
+                          <span style={{ fontSize: "24px" }}> &hearts;</span>
+                        </Button>
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+              );
+            })}
+        </div>
+      )}
     </>
   );
 }
